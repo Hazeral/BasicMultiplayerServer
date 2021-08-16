2aee7a970d5def4f32f751ffea03c1d142a8c4bf
Muhammad Usman <usman.hxz@gmail.com>
2021-08-16 16:59:20 +0100

    More accurate map bounds and wider range of coordinate generation

--- MultiplayerTestServer/Map.cs
+++ MultiplayerTestServer/Map.cs
@@ -6,15 +6,15 @@ namespace MultiplayerTestServer
     {
         public static float[,] Bounds =
         {
-            { -8, 8 }, // X
-            { -4, 4 }  // Y
+            { -8.59f, 8.6f }, // X
+            { -4.695f, 4.73f }  // Y
         };
 
         public static float[] RandomCoordinates()
         {
             Random r = new Random();
-            int x = r.Next((int)Bounds[0, 0], (int)Bounds[0, 1]);
-            int y = r.Next((int)Bounds[1, 0], (int)Bounds[1, 1]);
+            float x = (float)(r.NextDouble() * (Bounds[0, 1] - Bounds[0, 0]) + Bounds[0, 0]);
+            float y = (float)(r.NextDouble() * (Bounds[1, 1] - Bounds[1, 0]) + Bounds[1, 0]);
 
             return new float[] { x, y };
         }
